33d48902171f383212b5e0b457ca95a4791a1572
Juwan Lee <ljw05w5@gmail.com>
2023-06-09 10:20:44 +0900

    mongodb로 로그인 구현하기

--- DatabaseExample/app.js
+++ DatabaseExample/app.js
@@ -44,128 +44,119 @@ app.use(expressSession({
 
 //===== 데이터베이스 연결 =====//
 
-// 몽고디비 모듈 사용
+var MongoClient = require('mongodb').MongoClient;
 
+var database;
+function connectDB() {
 
+// 데이터베이스 연결 정보
+  var databaseUrl = 'mongodb://127.0.0.1:27017/local';
 
-// 데이터베이스 객체를 위한 변수 선언
+  // 데이터베이스 연결
+  MongoClient.connect(databaseUrl, function(err, db) {
+    if(err) throw err;
+    
+    console.log('데이터베이스에 연결되었습니다. : ' + databaseUrl);
+    
+    // database 변수에 할당
+    database = db.db('local');
+    
+  });
+}
 
-
-//데이터베이스에 연결
-
-	// 데이터베이스 연결 정보
-	
-	
-	// 데이터베이스 연결
-	
-		
-		
-
-
-
-
-
-
-
-
-
+  
 
 //===== 라우팅 함수 등록 =====//
 
 // 라우터 객체 참조
-
+var router = express.Router();
 
 // 로그인 라우팅 함수 - 데이터베이스의 정보와 비교
+router.route('/process/login').post(function(req, res) {
+
+  console.log('/process/login 호출됨.');
+
+  // 요청 파라미터 확인
+  var paramId = req.body.id || req.query.id;
+  var paramPassword = req.body.password || req.query.password;
+  console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword);
+
+  // 데이터베이스 객체가 초기화된 경우, authUser 함수 호출하여 사용자 인증
+  if (database) {
+    authUser(database, paramId, paramPassword, function(err, docs) {
+      if (err) {throw err;} // 파라메터 전달에 오류...
+      
+      // 조회된 레코드가 있으면 성공 응답 전송
+      if (docs) {
+        console.dir(docs);
+
+        // 조회 결과에서 사용자 이름 확인
+        var username = docs[0].name;
+        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+        res.write('<h1>로그인 성공</h1>');
+        res.write('<div><p>사용자 아이디 : ' + paramId + '</p></div>');
+        res.write('<div><p>사용자 이름 : ' + username + '</p></div>');
+        res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+        res.end();
+        
+      } else { // 조회된 레코드가 없는 경우 실패 응답 전송
+        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+        res.write('<h1>로그인 실패</h1>');
+        res.write('<div><p>아이디와 패스워드를 다시 확인하십시오.</p></div>');
+        res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+        res.end();
+      }// end- if (docs) {
+
+    }); // end- authUser(database, paramId, paramPassword, function(err, docs) {
+  } else { // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송, db연결 오류...
+    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+    res.write('<h2>데이터베이스 연결 실패</h2>');
+    res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>');
+    res.end();
+  } // end- if (database) {
+}); // end- router.route('/process/login').post(function(req, res) {
 
 
-
-    // 요청 파라미터 확인
-
-
-	
-
-    
-    // 데이터베이스 객체가 초기화된 경우, authUser 함수 호출하여 사용자 인증
-
-			
-				
-				
-				// 조회된 레코드가 있으면 성공 응답 전송
-
-
-
-    // 조회 결과에서 사용자 이름 확인
-
-
-
-
-
-
-
-
-			 // 조회된 레코드가 없는 경우 실패 응답 전송
-
-
-
-
-
-
-
-
-				         // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
-
-
-
-
-
-
-
-													
 // 라우터 객체 등록
 app.use('/', router);
 
 
 // 사용자를 인증하는 함수
+var authUser = function(database, id, password, callback) {
+  console.log('authUser 호출됨.');
+  
+  var users = database.collection('users');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  users.find({"id" : id, "password" : password}).toArray(function(err, docs) {
+    if(err) {
+      callback(err, null);
+      return;
+    }
+    if(docs.length > 0) {
+      console.log('아이디 [%s], 비밀번호 [%s]가 일치하는 사용자 찾음.', id, password);
+      callback(null, docs);
+    } else {
+      console.log("일치하는 사용자를 찾지 못함.");
+      callback(null, null);
+    }
+  });
+}
 
 
 // 404 에러 페이지 처리
 var errorHandler = expressErrorHandler({
- static: {
-   '404': './public/404.html'
- }
+  static: {
+    '404': './public/404.html'
+  }
 });
-
+ 
 app.use( expressErrorHandler.httpError(404) );
 app.use( errorHandler );
+ 
 
-
-// Express 서버 시작
-http.createServer(app).listen(app.get('port'), function(){
+//===== 서버 시작 =====//
+http.createServer(app).listen(app.get('port'), function() {
   console.log('서버가 시작되었습니다. 포트 : ' + app.get('port'));
-
-  // 데이터베이스 연결을 위한 함수 호출
-
-   
+  // 데이터베이스 연결
+  connectDB();
 });
